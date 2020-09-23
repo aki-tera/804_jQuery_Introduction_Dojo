2a2122f1d543da112114c1c0b2f7e8eb82e3d358
aki-tera <aki.tera@gmail.com>
2020-09-24 08:53:03 +0900

    20200924

--- Chapter08/cgi-bin/sample3.py
+++ Chapter08/cgi-bin/sample3.py
@@ -9,29 +9,6 @@ import cgitb
 cgitb.enable(display=1)
 
 
-def main02():
-
-    # おまじない
-    print('Content-type: application/json; charset=UTF-8')
-    print("\r\n\r\n")
-
-    with open("log.txt", mode="w") as f:
-        f.write(str(datetime.datetime.now())+":START\n")
-
-        # POSTデータの判定
-        if (os.environ['REQUEST_METHOD'] != "POST"):
-            f.write(str(datetime.datetime.now())+":METHOD不正\n")
-        else:
-            f.write(str(datetime.datetime.now())+":METHOD=POST\n")
-
-        # 標準入力で対応する場合
-        # 但し連続した文字列になり処理が面倒
-        # 例）myname=yamada&mypass=abcde&myclss=level2
-        # 通常は辞書形式
-        data = sys.stdin.read()
-        f.write(str(datetime.datetime.now()) + str(data))
-
-
 def main01():
     # おまじない
     print('Content-type: application/json; charset=UTF-8')
@@ -48,25 +25,21 @@ def main01():
 
         # データ取得
         form = cgi.FieldStorage()
-        # データを切り分けする場合の方法
-
-        # その1：個別に取り出したいとき
-        # form_key = form.getfirst("myname")
-        # form_pass = form.getfirst("mypass")
 
         f.write(str(datetime.datetime.now())+":DATA取得\n")
+        f.write(str(form)+"\n")
+
+        dict = {"result": "OK", "message": "python is very good!!"}
 
         # その2：キーが分からない場合
         for key in form.keys():
             variable = str(key)
             value = str(form.getvalue(variable))
             f.write(str(key)+":"+value+"\n")
+            dict[variable] = value
 
         f.write(str(datetime.datetime.now())+":送信\n")
 
-        # jsonデータを作成する
-        # まずは辞書形式を作ってjson形式に変換する
-        dict = {"result": "OK", "message": "python is good!!"}
         json_data = json.dumps(dict)
 
         f.write(str(json_data)+"\n")
@@ -93,23 +66,14 @@ def main():
     # データ取得
     form = cgi.FieldStorage()
 
-    # データを切り分けする場合の方法
+    dict = {"result": "OK", "message": "python is very good!!"}
 
-    # その1：個別に取り出したいとき
-    # form_key = form.getfirst("myname")
-    # form_pass = form.getfirst("mypass")
-
-    # データを切り分けする場合の方法
-    # その2：キーが分からない場合
     for key in form.keys():
         variable = str(key)
         value = str(form.getvalue(variable))
+        dict[variable] = value
 
-    # jsonデータを作成する
-    # まずは辞書形式を作ってjson形式に変換する
-    dict = {"result": "OK", "message": "python is very good!!"}
     json_data = json.dumps(dict)
-
 
     print(json_data)
     print("\r\n")
@@ -117,7 +81,7 @@ def main():
 
 if __name__ == "__main__":
 
-    main()
+    main01()
 
     # main()にログ出力する機能付き
     # main01()
